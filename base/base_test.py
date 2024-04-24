3e5e45dc25e91ab83f40513eb2af80b7b71af8ab
strokayov <strokanyov@yandex.ru>
2024-04-24 17:15:08 +0300

    First CI push

---
+++ base/base_test.py
@@ -0,0 +1,20 @@
+from pages.login_page import LoginPage
+from pages.dashboard_page import DashboardPage
+from pages. personal_page import PersonalPage
+import pytest
+from config.data import Data
+
+class BaseTest:
+
+    login_page: LoginPage
+    dashboard_page: DashboardPage
+    personal_page: PersonalPage
+
+
+    @pytest.fixture(autouse=True)
+    def setup(self, request, driver):
+        request.cls.driver = driver
+        request.cls.data = Data()
+        request.cls.login_page = LoginPage(driver)
+        request.cls.dashboard_page = DashboardPage(driver)
+        request.cls.personal_page = PersonalPage(driver)
